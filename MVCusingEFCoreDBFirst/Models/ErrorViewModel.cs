cb0d31dd4187e23c9f32fb4006ccae8108d1412c
whernandez94 <grupogd.whernandez@gmail.com>
2025-03-25 21:08:18 -0600

    Agregar archivos de proyecto.

---
+++ MVCusingEFCoreDBFirst/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCusingEFCoreDBFirst.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ MVCusingEFCoreDBFirst/SQLQuery.sql
@@ -0,0 +1,18 @@
+﻿CREATE DATABASE Company
+GO
+USE Company
+GO
+CREATE TABLE Employee(
+Id int IDENTITY(1,1) PRIMARY KEY,
+FirstName nvarchar(50) NOT NULL,
+LastName nvarchar(50),
+Salary numeric(18, 0),
+Department nvarchar(50)
+)
+GO
+INSERT INTO Employee VALUES('Pranaya', 'Rout', 10000, 'IT');
+INSERT INTO Employee VALUES('Priyanka', 'Dewangan', 20000, 'HR');
+INSERT INTO Employee VALUES('Preety', 'Tiwari', 30000, 'HR');
+INSERT INTO Employee VALUES('Tarun', 'Mallick', 40000, 'IT');
+INSERT INTO Employee VALUES('Anurag', 'Mohanty', 50000, 'IT');
+GO
