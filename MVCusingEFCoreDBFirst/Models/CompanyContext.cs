d8843302fdce6142310fc9187361e46dba633294
whernandez94 <grupogd.whernandez@gmail.com>
2025-03-25 22:09:50 -0600

    Auto generate controller and views

--- MVCusingEFCoreDBFirst/Models/CompanyContext.cs
+++ MVCusingEFCoreDBFirst/Models/CompanyContext.cs
@@ -18,8 +18,8 @@ public partial class CompanyContext : DbContext
     public virtual DbSet<Employee> Employees { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=WHERNANDEZ\\SQLEXPRESS;Initial Catalog=Company;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+    {
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
--- MVCusingEFCoreDBFirst/Program.cs
+++ MVCusingEFCoreDBFirst/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using MVCusingEFCoreDBFirst.Models;
+
 namespace MVCusingEFCoreDBFirst
 {
     public class Program
@@ -8,9 +11,9 @@ namespace MVCusingEFCoreDBFirst
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
-
+            //Adding DBContext Service
+            builder.Services.AddDbContext<CompanyContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DBConnection")));
             var app = builder.Build();
-
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
@@ -18,18 +21,13 @@ namespace MVCusingEFCoreDBFirst
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
-
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
             app.Run();
         }
     }
