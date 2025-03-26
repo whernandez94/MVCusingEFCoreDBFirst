afa7ad5b06ef4e91ac3cfe6c61f394330fcdecf9
whernandez94 <grupogd.whernandez@gmail.com>
2025-03-25 21:27:02 -0600

    Execute command Scaffold-Dbcontext

---
+++ MVCusingEFCoreDBFirst/Models/CompanyContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVCusingEFCoreDBFirst.Models;
+
+public partial class CompanyContext : DbContext
+{
+    public CompanyContext()
+    {
+    }
+
+    public CompanyContext(DbContextOptions<CompanyContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=WHERNANDEZ\\SQLEXPRESS;Initial Catalog=Company;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Employee__3214EC07002672D0");
+
+            entity.ToTable("Employee");
+
+            entity.Property(e => e.Department).HasMaxLength(50);
+            entity.Property(e => e.FirstName).HasMaxLength(50);
+            entity.Property(e => e.LastName).HasMaxLength(50);
+            entity.Property(e => e.Salary).HasColumnType("numeric(18, 0)");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ MVCusingEFCoreDBFirst/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MVCusingEFCoreDBFirst.Models;
+
+public partial class Employee
+{
+    public int Id { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string? LastName { get; set; }
+
+    public decimal? Salary { get; set; }
+
+    public string? Department { get; set; }
+}
